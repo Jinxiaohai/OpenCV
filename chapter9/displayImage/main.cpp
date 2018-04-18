dd000fcf582cafd046da427967c4ebb81d7b42a5
Jinxiaohai <xiaohaijin@outlook.com>
2018-04-18 08:34:43 +0800

    test

---
+++ chapter9/displayImage/main.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char **argv) {
+  //  cv::namedWindow("xiaohai", cv::WINDOW_AUTOSIZE);
+  //  cv::Mat img =
+  // cv::imread("/home/xiaohai/Github/learnOpenCV/Picture/test.jpg");
+  //  cv::imshow("xiaohai", img);
+  //  while (true) {
+  //    if (cv::waitKey(100000) == 27) {
+  //      break;
+  //    }
+  //    cv::destroyAllWindows();
+  //  }
+
+  return 0;
+}
